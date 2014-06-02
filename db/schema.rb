47e57e8af4f9f49709bcaf667b19e6205d72519a
nikopapa81 <nikopapa81@gmail.com>
2014-06-02 01:40:49 -0500

    Added username and avatar. Cleaned up NavBar.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140602021609) do
+ActiveRecord::Schema.define(version: 20140602054418) do
 
   create_table "activity_categories", force: true do |t|
     t.string   "activity_category"
@@ -68,6 +68,8 @@ ActiveRecord::Schema.define(version: 20140602021609) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
+    t.string   "avatar_url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
