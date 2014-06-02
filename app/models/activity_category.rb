f0efd44854381f702390fefd012a22c4cb9800d2
nikopapa81 <nikopapa81@gmail.com>
2014-06-01 19:25:33 -0500

    Relationships & validations for Activity Category

--- app/models/activity_category.rb
+++ app/models/activity_category.rb
@@ -1,2 +1,9 @@
 class ActivityCategory < ActiveRecord::Base
+
+has_many :activity_lines
+has_many :food_items, :through => :activity_lines, :source => :activity_line_id
+has_many :food_categories, :through => :activity_lines, :source => :activity_line_id
+
+validates :activity_category, :presence => true
+
 end
